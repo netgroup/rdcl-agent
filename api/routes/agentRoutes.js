17d72f75ba5d79658e8aaed4872537bd66207196
Francesco Lombardo <franclombardo@gmail.com>
2017-04-10 09:47:04 +0200

    agent route

--- api/routes/agentRoutes.js
+++ api/routes/agentRoutes.js
@@ -1,15 +1,42 @@
-var express = require('express')
-  , router = express.Router()
+var express = require('express'),
+router = express.Router();
 
-router.get('/deployments', function(req, res) {
-	
-})
+var MODULE_NAME = 'route/agentRoutes';
+
+// a middleware function with no mount path. This code is executed for every request to the router
+router.use(function (req, res, next) {
+    console.log(MODULE_NAME, req.method, req.originalUrl, Date.now());
+    next();
+});
+
+// execute callback logErrors when an error occur
+router.use(logErrors);
 
 
+// get all info about a deployment
+router.get('/deployments/:id', function(req, res) {
+
+});
+
+// get all info about a deployment
+router.delete('/deployments/:id', function(req, res) {
+
+});
+
+//action on a specific deployment
+router.post('/deployments/:id/:action', function(req, res) {
+
+});
+
+// get info about agent
 router.get('/status', function(req, res) {
 
 
-})
+});
 
+function logErrors (err, req, res, next) {
+  console.error(MODULE_NAME, err.stack);
+  next(err);
+}
 
-module.exports = router
+module.exports = router;
